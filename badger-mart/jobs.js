e666f149dd0162ce90f8722dbc91014f0ee66b8b
Abby <Patron@Abbys-MacBook-Air.local>
2024-12-27 22:58:13 -0600

    completed hw1

--- badger-mart/jobs.js
+++ badger-mart/jobs.js
@@ -2,4 +2,17 @@ function submitApplication(e) {
     e.preventDefault(); // You can ignore this; prevents the default form submission!
 
     // TODO: Alert the user of the job that they applied for!
+    
+    const jobs = document.getElementsByName('job');
+    let flag = false;
+    for (let item of jobs) {
+        if (item.checked) {
+            flag = true;
+            let job = item.value;
+            alert("Thank you for applying to be a " + job + "!");
+        }
+    }
+    if (flag == false) {
+        alert("Please select a job!");
+    }
 }
